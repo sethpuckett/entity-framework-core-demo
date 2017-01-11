b33a5ff72c3548d06ddc293c6b69890cdb67380e
Seth Puckett <puckett.seth@gmail.com>
2017-01-11 17:27:38 -0500

    adding model classes

---
+++ Models/Tag.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace entity_framework_core_demo.Models
+{
+    public class Tag
+    {
+        public Tag()
+        {
+        }
+    }
+}
