8932c255ed93a6ee3029919088495207c00dfc16
Seth Puckett <puckett.seth@gmail.com>
2017-01-26 21:07:30 -0500

    updating controller

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -24,24 +24,23 @@ namespace entity_framework_core_demo.Controllers
         {
             return Ok(this.context.Todos
                 .Include(t => t.Status)
-                // TODO: this isn't working
-                /*.Include(t => t.TodoTags).ThenInclude(t => t.Tag) */
+                .Include(t => t.TodoTags).ThenInclude(t => t.Tag) 
                 );
         }
 
         [HttpPost]
-        public IActionResult AddTodo([FromBody] Todo newTodo)
+        public async Task<IActionResult> AddTodo([FromBody] Todo newTodo)
         {
             newTodo.Status = this.context.Statuses.First(s => s.Title == "New");
 
             this.context.Todos.Add(newTodo);
-            this.context.SaveChangesAsync();
+            await this.context.SaveChangesAsync();
 
             return Ok();
         }
 
         [HttpPost, Route("{todoId}/tag")]
-        public IActionResult AddTag(int todoId,[FromBody] string tagTitle)
+        public async Task<IActionResult> AddTag(int todoId,[FromBody] string tagTitle)
         {
             var todo = this.context.Todos.First(t => t.Id == todoId);
             var tag = this.context.Tags.FirstOrDefault(t => t.Title == tagTitle);
@@ -55,40 +54,40 @@ namespace entity_framework_core_demo.Controllers
             var todoTag = new TodoTag { Todo = todo, Tag = tag };
             this.context.TodoTags.Add(todoTag);
 
-            this.context.SaveChangesAsync();
+            await this.context.SaveChangesAsync();
 
             return Ok();
         }
 
         [HttpPost, Route("{todoId}/status")]
-        public IActionResult UpdateStatus(int todoId,[FromBody] string statusTitle)
+        public async Task<IActionResult> UpdateStatus(int todoId,[FromBody] string statusTitle)
         {
             var todo = this.context.Todos.First(t => t.Id == todoId);
             var status = this.context.Statuses.First(s => s.Title == statusTitle);
 
             todo.Status = status;
 
-            this.context.SaveChangesAsync();
+            await this.context.SaveChangesAsync();
 
             return Ok();
         }
 
         [HttpDelete, Route("{todoId}")]
-        public IActionResult DeleteTodo(int todoId)
+        public async Task<IActionResult> DeleteTodo(int todoId)
         {
             var todo = this.context.Todos.First(t => t.Id == todoId);
             this.context.Todos.Remove(todo);
-            this.context.SaveChangesAsync();
+            await this.context.SaveChangesAsync();
 
             return Ok();
         }
 
         [HttpDelete, Route("finished")]
-        public IActionResult DeleteFinishedTodos()
+        public async Task<IActionResult> DeleteFinishedTodos()
         {
             var todos = this.context.Todos.Where(t => t.Status.Title == "Finished");
             this.context.Todos.RemoveRange(todos);
-            this.context.SaveChangesAsync();
+            await this.context.SaveChangesAsync();
 
             return Ok();
         }
